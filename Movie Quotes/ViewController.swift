4cbdfd1ebad76fff006fd0cee785d7c3adc427c5
lamia <lamia.fd123@gmail.com>
2021-12-21 10:57:46 +0300

    Initial Commit

---
+++ Movie Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Quotes
+//
+//  Created by لمياء فالح الدوسري on 17/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
